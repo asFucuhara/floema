a37fef760277425796b2c16752106a5d2abfe5ab
André Shindi Fucuhara <asfucuhara@live.com>
2023-06-16 18:35:17 -0300

    feat: js base

--- app/index.js
+++ app/index.js
@@ -1,7 +1,73 @@
+import { each } from 'lodash'
+import About from './pages/About'
+import Collections from './pages/Collections'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+
 class App {
   constructor () {
-    console.log('app')
+    this.createContent()
+    this.createPages()
+
+    this.addLinkListeners()
+  }
+
+  createPages () {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+
+    this.page = this.pages[this.template]
+    this.page.create()
+    this.page.show()
+  }
+
+  createContent () {
+    this.content = document.querySelector('.content')
+    this.template = this.content.getAttribute('data-template')
+  }
+
+  async onChange (url) {
+    await this.page.hide()
+    const request = await window.fetch(url)
+
+    if (request.status === 200) {
+      const html = await request.text()
+      const div = document.createElement('div')
+
+      div.innerHTML = html
+
+      const divContent = div.querySelector('.content')
+
+      this.template = divContent.getAttribute('data-template')
+
+      this.content.innerHTML = divContent.innerHTML
+      this.content.setAttribute('data-template', this.template)
+
+      this.page = this.pages[this.template]
+      this.page.create()
+      await this.page.show()
+    } else {
+      console.log('error')
+    }
+  }
+
+  addLinkListeners () {
+    const links = document.querySelectorAll('a')
+
+    each(links, link => {
+      link.onclick = event => {
+        event.preventDefault()
+
+        const { href } = link
+
+        this.onChange(href)
+      }
+    })
   }
 }
 
-new App();
+new App()
---
+++ app/pages/Home/index.js
@@ -0,0 +1,18 @@
+import Page from '../../classes/Page'
+
+export default class Home extends Page {
+  constructor () {
+    super({
+      id: 'home',
+      element: '.home',
+      elements: {
+        button: '.home__link',
+        navigation: document.querySelector('.navigation')
+      }
+    })
+  }
+
+  create () {
+    super.create()
+  }
+}
